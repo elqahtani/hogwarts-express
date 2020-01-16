133c500750dc7b51b3feebd79776a4a62b3b877a
Elqahtani <root@rm-rf.web.id>
2020-01-16 10:25:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server = http.createServer(function(request, response) {
 	response.statusCode = 200; 
 	response.setHeader('Content-Type', 'text/plain'); 
 	response.end('Welcome to the Kings Cross Station Platform'); });
-	server.listen(3000, function() { console.log('Server running on port 3000');
+	server.listen(31337, function() { console.log('Server running on port 31337');
 });
 server.listen(31337, function() { 
 	console.log('Server running on port 31337');
